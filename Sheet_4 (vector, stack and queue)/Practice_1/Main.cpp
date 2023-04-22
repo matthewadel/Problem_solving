0b356e4dcb6b4ba6406205828793b6f52266de7b
matthewadel <matthewadel69@yahoo.com>
2023-04-22 15:46:42 +0200

    Practice_1

---
+++ Sheet_4 (vector, stack and queue)/Practice_1/Main.cpp
@@ -0,0 +1,31 @@
+https : // leetcode.com/problems/time-needed-to-buy-tickets/description/
+#include <bits/stdc++.h>
+        using namespace std;
+int timeRequiredToBuy(vector<int> &tickets, int k)
+{
+
+  int totalTime = 0;
+  int i = 0;
+  while (i < tickets.size())
+  {
+    if (i <= k)
+      totalTime += (tickets[i] <= tickets[k]) ? tickets[i] : tickets[k];
+    else
+      totalTime += tickets[i] < tickets[k] ? tickets[i] : (tickets[k] - 1);
+
+    // cout<<totalTime<<endl;
+    i++;
+  }
+
+  return totalTime;
+}
+
+int main()
+{
+
+  vector<int> tickets = {2, 3, 2};
+  int k = 2;
+
+  cout << timeRequiredToBuy(tickets, k) << endl;
+  return 0;
+}
