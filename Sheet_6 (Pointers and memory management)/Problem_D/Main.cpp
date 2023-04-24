13e904fdbf04a31797e5475ab47c67b874d55883
matthewadel <matthewadel69@yahoo.com>
2023-04-24 15:54:20 +0200

    Problem_C

---
+++ Sheet_6 (Pointers and memory management)/Problem_D/Main.cpp
@@ -0,0 +1,25 @@
+// https://vjudge.net/contest/539018#problem/C
+// https://codeforces.com/problemset/problem/560/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  long long number_of_inputs, temp;
+  long long sum_positive = 0, sum_negaitive = 0;
+cin>>number_of_inputs;
+  for (size_t i{0}; i < number_of_inputs; i++)
+  {
+    cin >> temp;
+    if (temp > 0)
+      sum_positive += temp;
+    else
+      sum_negaitive += temp;
+  }
+
+  cout << sum_positive - sum_negaitive << endl;
+
+  return 0;
+}
