e5146c326a3aae3c1da8410ff90ff3a1ecb75096
matthewadel <matthewadel69@yahoo.com>
2023-04-24 17:20:18 +0200

    Problem_G

---
+++ Sheet_6 (Pointers and memory management)/Problem_G/Main.cpp
@@ -0,0 +1,25 @@
+// https://vjudge.net/contest/539018#problem/F
+// https://codeforces.com/problemset/problem/337/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string s;
+  cin >> s;
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (s[i] == '0')
+    {
+      s.erase(i, 1);
+      break;
+    }
+    else if (s[i] != '0' && i == s.size() - 1)
+    {
+      s.erase(s.size() - 1);
+    }
+  }
+
+  cout << s << endl;
+}
